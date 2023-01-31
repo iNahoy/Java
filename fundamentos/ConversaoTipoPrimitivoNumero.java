7beb8aef3062d8454db69861760683520dd64398
Jorge Yohan <116468195+iNahoy@users.noreply.github.com>
2023-01-31 18:32:24 -0300

    Add files via upload

---
+++ fundamentos/ConversaoTipoPrimitivoNumero.java
@@ -0,0 +1,15 @@
+package fundamentos;
+
+public class ConversaoTipoPrimitivoNumero {
+	public static void main(String[] args) {
+		double a = 1;                     // Conversão Implícita
+		System.out.println(a);
+		
+		float b = (float) 1.123456789999; // Conversão Explícita (CAST)
+		System.out.println(b);
+		
+		int c = 360; 					  // Conversão Explícita (CAST)
+		byte d = (byte) c;
+		System.out.println(d);
+	}
+}
