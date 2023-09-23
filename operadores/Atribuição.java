1e52fe9324173785d579c861079b9da172f98efa
Jorge Yohan <116468195+iNahoy@users.noreply.github.com>
2023-09-23 02:53:01 -0300

    Add files via upload

---
+++ operadores/Atribuição.java
@@ -0,0 +1,17 @@
+package operadores;
+
+public class Atribuição {
+	public static void main(String[] args) {
+		int a = 3;
+		int b = a;
+		int c = a + b;
+		
+		c += b; // c = c + b;
+		c *= b; // c = c * b;
+		c -= a; // c = c - a;
+		c /= a; // c = c / a;
+		
+		c++;    // c = c + 1;
+		System.out.println(c);
+	}
+}
