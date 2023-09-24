2c45c3ee41c36bd94396d44afe52b087a4d705a3
Jorge Yohan <116468195+iNahoy@users.noreply.github.com>
2023-09-24 01:21:03 -0300

    Add files via upload

---
+++ operadores/OperadoresRelacionais.java
@@ -0,0 +1,17 @@
+package operadores;
+
+public class OperadoresRelacionais {
+	public static void main(String[] args) {
+		int a = 24;
+		int b = 'a';
+		
+		System.out.println(a == b);
+		
+		System.out.println(3 >  4);
+		System.out.println(3 >= 3);
+		System.out.println(3 <  7);
+		System.out.println(3 <= 7);
+		System.out.println(9 != 7);
+		
+	}
+}
